4df13a276a855fdca4e2b5b083f6acd3b700ce5d
zyl0462 <109072803+zyl0462@users.noreply.github.com>
2023-09-06 16:37:57 +0800

    更新 gpt.js

--- gpt.js
+++ gpt.js
@@ -4,7 +4,6 @@ if(method == 'GET'){
     let new_url =  $request.url.replace('vip=false','vip=true');
     $done({url:new_url});
 }else{
-    let body =$request.body;
-    let new_body = body.replace('vip=false','vip=true');
+    let new_body = $request.body.replace('vip=false','vip=true');
     $done({ body:new_body});
 }
